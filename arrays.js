4b0e124b261679f9c7b55c7356ba87d4da993baf
edwin rivera <waehake@gmail.com>
2018-01-17 01:45:11 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,46 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, item) {
+  var otherarray = [item, ...arr];
+  return otherarray;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, item) {
+  arr.unshift(item);
+  return arrl
+}
+
+function addElementToEndOfArray(arr, element) {
+  var otherarray = arr.push(element);
+  return otherarray;
+}
+
+function destructivelyAddElementToEndOfArray(arr, element) {
+  arr = [...arr, element];
+  return arr;
+}
+
+function accessElementInArray(arr, element) {
+  return arr[element];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  arr.slice(1);
+  return arr;
+}
+
+function destructivelyRemoveElemenrFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  arr.slice(0, arr.length - 1);
+  return arr;
+}
