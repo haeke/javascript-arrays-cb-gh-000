7e8278468ee0082de5f695eaf1c014e68dd6a6cc
edwin rivera <waehake@gmail.com>
2018-01-17 01:54:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,6 +41,5 @@ function destructivelyRemoveElementFromEndOfArray(arr) {
 }
 
 function removeElementFromEndOfArray(arr) {
-  arr.slice(0, arr.length - 1);
-  return arr;
+  return arr.slice(0, arr.length - 1);
 }
